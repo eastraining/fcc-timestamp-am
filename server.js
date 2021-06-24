95942336ef61f09b7111ede39ffcc32b52d211c4
eastraining <heeyudong@gmail.comm>
2021-06-24 23:20:25 +0800

    minor tweaks

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// API date checker endpoint
+app.get("/api/:date", function (req, res) {
+  
 });
 
 

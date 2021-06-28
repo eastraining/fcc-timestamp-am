ca91763f529567e2264b680cea806472e0301dd0
eastraining <heeyudong@gmail.comm>
2021-06-28 17:50:43 +0800

    passed tests in repl.it

--- server.js
+++ server.js
@@ -5,6 +5,8 @@
 var express = require('express');
 var app = express();
 var dayjs = require('dayjs');
+var utc = require('dayjs/plugin/utc');
+dayjs.extend(utc);
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -20,15 +22,35 @@ app.get("/", function (req, res) {
 });
 
 
-// API date checker endpoint
-app.get("/api/:date", function (req, res) {
-  let inputDate = req.params.date;
-  if (!req.params.date) {
+// API date endpoint returns following timestamp
+// { unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" }
+app.get("/api/", function (req, res) {
+  res.json({
+    unix: dayjs().valueOf(),
+    utc: dayjs().utc().format('ddd, DD MMM YYYY HH:mm:ss [GMT]')
+  });
+});
 
-  } else if (dayjs(inputDate).isValid()) {
-    // TODO
+app.get("/api/:date", function (req, res) {
+  let input = req.params.date;
+  if (!dayjs(input).isValid()) {
+    res.json({ error: "Invalid Date" });
   } else {
-    
+    if ((/\D/).test(input)) {
+      res.json({
+        unix: dayjs(input).valueOf(),
+        utc: dayjs(input).utc().format('ddd, DD MMM YYYY HH:mm:ss [GMT]')
+      })
+    } else if (input.length === 10 || input.length === 13) {
+      let unixDate = input.length === 10 ? Number(input) * 1000 : Number(input);
+      let utcDate = new Date(unixDate);
+      res.json({
+        unix: unixDate,
+        utc: dayjs(utcDate).utc().format('ddd, DD MMM YYYY HH:mm:ss [GMT]')
+      })
+    } else {
+      res.json({ error: "Invalid Date" });
+    } 
   }
 });
 
